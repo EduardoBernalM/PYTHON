b937b9f9aac7b4590ae5e25ac56ad53c3d69692e
EduardoBernalM <bernalmoralesluiseduardo@gmail>
2022-10-26 15:39:16 -0500

    python

---
+++ algoritmo.py
@@ -0,0 +1,27 @@
+galones = float(input("cantidad"))
+galon_litro=3.78
+precio=20
+litros=galon_litro*galones
+total=litros*precio_litro
+print("total a pagar:",total)
+
+
+/*coordenada*/
+x1=float(input("x1"))
+x2=float(input("y1"))
+y1=float(input("x2"))
+y2=float(input("y2"))
+distancia=((x1-x2)**2+(y1-y2)**2)**(1/2)
+print ("la distancia es",distancia)
+
+
+*/bar/*
+hora_e=int(input("hora entrada"))
+min_e=int(input("minutos entrada"))
+hora_s=int(input("hora salida"))
+min_s=int(input("minuto salida"))
+min_totales=(hora_s-hota_e)*60+min_s-min_e
+hora_completa=min_totales//60
+min_sub=min_totales%60
+total_pagar=hora_completa*50+min_sub*1.5
+print("tiempo a pagar",tiempo_a_pagar)
