0791264285dd899dfe1052766ab92ada95419c9c
EduardoBernalM <bernalmoralesluiseduardo@gmail>
2022-10-27 16:11:37 -0500

    python

--- algoritmo.py
+++ algoritmo.py
@@ -6,7 +6,7 @@ total=litros*precio_litro
 print("total a pagar:",total)
 
 
-/*coordenada*/
+#coordenada
 x1=float(input("x1"))
 x2=float(input("y1"))
 y1=float(input("x2"))
@@ -15,7 +15,7 @@ distancia=((x1-x2)**2+(y1-y2)**2)**(1/2)
 print ("la distancia es",distancia)
 
 
-*/bar/*
+#bar
 hora_e=int(input("hora entrada"))
 min_e=int(input("minutos entrada"))
 hora_s=int(input("hora salida"))
@@ -33,3 +33,36 @@ area=ancho_base*largo_base+ancho_base*altura*2+largo_base*altura*2+ancho_base*al
 precio=0.5
 tatal_pagar=area*precio*N
 print("total",total_pagar)
+
+
+
+#condicionales
+boletos=int(input("Boleto"))
+precio=350
+total=precio*boletos
+if boletos >10:
+    total=total*0.9
+elif boletos >=5:
+    total=total*0.95
+
+#2
+b_m=int(input("Boleto menores"))
+b_M=int(input("Boleto mayores"))
+n_b=b_m+b_M
+precio_m=250
+precio_M=350
+pago_m=precio_m*b_m
+if b_m>=5:
+    pago_m*=0.9
+pago_M=precio_M*b_M
+total=pago_M+pago_m
+if n_b>=10:
+    total*=0.9
+elif n_b>=5:
+    total*=0.95
+print (f"boletos menores: {b_m}"
+f"subtotal= {pago_m}\n"
+f"boleto mayores: {b_M}"
+f"subtotal mayores= {pago_M}"
+f"total= {total}\n"
+f"total+iva= {total_iva}")
